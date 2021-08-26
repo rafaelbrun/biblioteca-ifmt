b28d99fe8f4fe08ced3f61c741d3d325cab20185
Rafael <rafaelbrungolin@gmail.com>
2021-08-26 09:28:34 -0400

    Estiliza tela de perfil

--- src/pages/app/profile/index.tsx
+++ src/pages/app/profile/index.tsx
@@ -1,22 +1,71 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import styles from './styles';
 import AppStatusBar from '../../../components/AppStatusBar';
 import { useAuth } from '../../../contexts/auth';
+import { AntDesign, Ionicons, MaterialIcons, SimpleLineIcons } from '@expo/vector-icons';
 
 export default function Profile() {
-	
-	const { signOut } = useAuth();
+
+	const { signOut, user } = useAuth();
 
 	function handleDeslogar() {
-		signOut();
+		Alert.alert(
+			'Sair',
+			'Deseja deslogar?',
+			[
+				{
+					text: 'Cancelar',
+					onPress: () => console.log('Cancel Pressed'),
+					style: 'cancel'
+				},
+				{
+					text: 'OK', onPress: () => {
+						signOut();
+					}
+				}
+			],
+			{ cancelable: false }
+		);
 	}
 
 	return (
 		<View style={styles.container}>
 			<AppStatusBar></AppStatusBar>
-			<Text>Profile</Text>
-			<Button title="deslogar" onPress={handleDeslogar}></Button>
+			<View>
+				<View style={styles.header}>
+					<Text style={styles.userNome}>{user.nome} - {user.matricula}</Text>
+				</View>
+				<View style={styles.body}>
+					<TouchableOpacity>
+						<View style={styles.itemContainer}>
+							<AntDesign name="book" size={20} color="black" />
+							<Text style={styles.itemText}>Reservas</Text>
+						</View>
+					</TouchableOpacity>
+					<View style={styles.divisor}></View>
+					<TouchableOpacity>
+						<View style={styles.itemContainer}>
+							<SimpleLineIcons name="exclamation" size={18} color="black" />
+							<Text style={styles.itemText}>Alertas</Text>
+						</View>
+					</TouchableOpacity>
+					<View style={styles.divisor}></View>
+					<TouchableOpacity>
+						<View style={styles.itemContainer}>
+							<MaterialIcons name="history" size={20} color="black" />
+							<Text style={styles.itemText}>Histórico</Text>
+						</View>
+					</TouchableOpacity>
+					<View style={styles.divisor}></View>
+					<TouchableOpacity onPress={handleDeslogar}>
+						<View style={styles.itemContainer}>
+							<Ionicons name="ios-exit-outline" size={20} color="black" />
+							<Text style={styles.itemText}>Sair</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+			</View>
 		</View>
 	);
 }
--- src/pages/app/profile/styles.tsx
+++ src/pages/app/profile/styles.tsx
@@ -1,62 +1,38 @@
 import { StyleSheet } from 'react-native';
-import { blue, lightBlue, gray } from '../../geral/styles'
+import { lightBlue, gray } from '../../geral/styles'
 
 export default StyleSheet.create({
-    logo: {
-        resizeMode: "contain",
-        width: 120,
-        height: 120,
-    },
-    textoLogo: {
-        color: blue,
-        fontSize: 36,
-        letterSpacing: 4
-    },
-    subTextoLogo: {
-        color: blue,
-        fontSize: 14,
-        letterSpacing: 2
-    },
     container: {
         flex: 1,
         backgroundColor: lightBlue,
     },
-    textoMenu: {
-        marginTop: 10,
-        color: gray
+    header: {
+        padding: 24
     },
-    layout: {
+    userNome: {
+        color: gray,
+        fontSize: 18,
+        letterSpacing: 1
+    },
+    body: {
+        backgroundColor: 'white'
+    },
+    itemContainer: {
+        display: 'flex',
         flexDirection: 'row',
+        padding: 16,
         alignItems: 'center',
+    },
+    itemText: {
+        letterSpacing: 1.5,
+        marginLeft: 12,
+        fontSize: 16
+    },
+    divisor: {
+        width: '95%',
         alignSelf: 'center',
-        marginTop: 12
-    },
-    versionText: {
-        color: "#D9DDDC",
-        margin: 4,
-    },
-    textDownView: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    containerGrid: {
-        alignItems: 'center',
-        margin: 0,
-        padding: 0,
-        marginTop: 12,
-    },
-    menuOp: {
-        justifyContent: 'center',
-        flex: 1,
-    },
-    imagem: {
-        resizeMode: "contain",
-        width: 60,
-        height: 60,
-    },
-    gridMenu: {
-        marginBottom: 60,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
+        height: 1,
+        borderBottomWidth: 1,
+        borderColor: '#efefef'
+    }
 });
